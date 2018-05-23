6843d69fa8f97c5b21b1375b1bc87da2097822f1
ndomitio <nicholas.domitio@gmail.com>
2018-05-23 15:52:25 -0400

    my projects added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Project';
 import './App.css';
 
 class App extends Component {
+constructor(){
+  super();
+  this.state ={
+    projects:[
+      {
+        title:'Business Website',
+        category: 'Web Design'
+      },{
+        title:'Social App',
+        category: 'Mobile Development'
+      },{
+        title:'Ecommerce Shopping Cart',
+        category: 'Web Development'
+      }
+    ]
+  }
+}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    My App
+    <Projects />
       </div>
     );
   }
---
+++ src/Components/Project.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div className="Projects">
+    My Projects
+      </div>
+    );
+  }
+}
+
+export default Projects;
