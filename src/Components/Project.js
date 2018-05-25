6a3dc04bb5da4b22e629af44158099159748e0fe
ndomitio <nicholas.domitio@gmail.com>
2018-05-25 14:13:22 -0400

    break

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 
 class Projects extends Component {
+
+
+
   render() {
+    let projectitems;
+    if(this.props.projects){
+      projectitems = this.props.projects.map(Project =>{
+        console.log(Project);
+      });
+        }
     console.log(this.props);
     return (
       <div className="Projects">
