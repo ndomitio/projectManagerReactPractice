079fac3b9a25bf30a2d4f5961007099e28491bc8
ndomitio <nicholas.domitio@gmail.com>
2018-05-29 11:46:49 -0400

    lifecyclecreated

--- src/App.js
+++ src/App.js
@@ -6,20 +6,26 @@ class App extends Component {
 constructor(){
   super();
   this.state ={
-    projects:[
-      {
-        title:'Business Website',
-        category: 'Web Design'
-      },{
-        title:'Social App',
-        category: 'Mobile Development'
-      },{
-        title:'Ecommerce Shopping Cart',
-        category: 'Web Development'
-      }
-    ]
+    projects:[]
   }
 }
+
+componentWillMount(){
+//lifecycle method
+//better implementation than putting data in constructor
+this.setState({projects:[{
+  title:'Business Website',
+  category: 'Web Design'
+},{
+  title:'Social App',
+  category: 'Mobile Development'
+},{
+  title:'Ecommerce Shopping Cart',
+  category: 'Web Development'
+}
+]});
+}
+
   render() {
     return (
       <div className="App">
