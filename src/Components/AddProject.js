536cb3274a9ee2df4c0eb389fc1e77ae632081eb
ndomitio <nicholas.domitio@gmail.com>
2018-05-30 10:42:06 -0400

    categorydropdownadded

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class AddProject extends Component {
+static defaultProps = {
+  categories : ['Web Design', 'Web Development', 'Mobile Development']
+}
+
   render() {
+    let categoryOptions = this.props.categories.map(category => {
+      return <option key = {category} value="category">{category}</option>
+    });
   return (
       <div >
 <h3>Add Project</h3>
+<form>
+<div>
+<lable>Title</lable><br/>
+<input type="text" ref="citle" />
+</div>
+
+<div>
+<lable>Category</lable><br/>
+<select ref="category">
+{categoryOptions}
+</select>
+</div>
+</form>
       </div>
     );
   }
